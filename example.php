b0747174981b21a0b8d1ba4aced2ca7c5ed7ddc4
ganjarsantoso <masganjar@gmail.com>
2014-09-26 09:20:51 +0700

    Update example.php

--- example.php
+++ example.php
@@ -2,25 +2,34 @@
 require_once('gs.vardump.php');
 
 $dump = new gsVarDump();
-
 $dump->useSkin('themes/default.css');
 
+$f = fopen(__FILE__,'r');
+fclose($f);
+
+$g = fopen(__FILE__,'r');
+
+$h = new stdClass(); 
+$h->H1 = 'h1';  
+$h->H2 = 'h2';  
+
+
 $array = array(
-			'A' => 'a', 
-			'B' => 
-				array(
-					'b1', 'b2', 'b3', array(
-										'b4_1', 
-										'b4_2'
-										), 
-					'b5', 'b6'
-				), 
-			'C' => 'c', 
-			'D' => array(
-						'D1' => 'd1', 
-						'D2' => 'd2'
-					)
+			'boolean' => true, 
+			'integer' => 234,
+			'float' => 5.89,
+			'string' => 'Hello World',
+			'array' => array(
+							'b1', 'b2', 'b3', array(),'b4', array('b5' => 'b51', 'b6' => 678)
+						), 
+			'object' => $h, 
+			'null' => null,
+			'resource' => $g,
+			'unknown' => $f
 		);
-		
-echo $dump->vardump($array);
-gs_vardump($array, 1);
+
+
+echo $dump->vardump($array,0);
+
+
+fclose($g); 
